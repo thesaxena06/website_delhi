90eca768194b8dd70881ef1bd0c0a951533ce681
thesaxena06 <thekumararyan847@gmail.com>
2021-11-19 10:36:00 +0530

    first commit

---
+++ scroll.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "components/Header";
+import ScrollIndicator from "components/ScrollIndicator";
+import ExternalInfo from "components/ExternalInfo";
+import AppConfig from "App.config";
+
+const ScrollIndicatorpage = () => {
+    return (
+        <Header title = "Scroll Indiactor"/>
+
+        <ExternalInfo
+          code=
+    )
+}
